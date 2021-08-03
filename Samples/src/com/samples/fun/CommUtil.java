8934587de726a3e9c6a9604d5c97e2b0fdd84831
Aravind Datta <Aravind Datta@192.168.0.34>
2021-08-03 12:32:53 -0400

    time change

--- Samples/src/com/samples/fun/CommUtil.java
+++ Samples/src/com/samples/fun/CommUtil.java
@@ -5,19 +5,23 @@ import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class CommUtil {
 	public static void computeStatsAndExit(long startTime, Scanner scanner, int sno, double correct,
 			double incorrect, String operation) {
 		scanner.close();
 		long elapsed = System.currentTimeMillis() - startTime;
-		long elapsedInSec = elapsed / 1000;
-		double timeInMin = elapsedInSec/60d;
+		long minutes = TimeUnit.MILLISECONDS
+			    .toMinutes(elapsed);
+		
+//		long elapsedInSec = elapsed / 1000;
+//		double timeInMin = elapsedInSec/60d;
 		DecimalFormat df = new DecimalFormat("#.##");
 		df.setRoundingMode(RoundingMode.CEILING);
 		SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyy");
 		System.out.println();
-		System.out.println(sdf.format(new Date()) + "\t"+operation+"\t" + df.format(timeInMin) + "\t"
+		System.out.println(sdf.format(new Date()) + "\t"+operation+"\t" + df.format(minutes) + "\t"
 				+ (sno - 1) + "\t" + Math.round(correct) + "\t" + Math.round(incorrect) + "\t"
 				+ Math.round((correct / (sno - 1)) * 100) + " %");
 		System.exit(1);
